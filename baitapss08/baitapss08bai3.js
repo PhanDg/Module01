c66aa7c43431610cf3f7261c6d6c3f783b1beee2
PhanDg <nguyenmacphan@gmail.com>
2024-07-31 20:26:29 +0900

    baitaps08

---
+++ baitapss08/baitapss08bai3.js
@@ -0,0 +1,5 @@
+var width = 100;
+var height = 100;
+var area = width * height;
+alert('Diện tích hình chữ nhật là :' + area);
+document.write('Diện tích hình chữ nhật là : ' +area);
---
+++ baitapss08/baitapss08bai5.js
@@ -0,0 +1,6 @@
+var hoahoc = 9;
+var vatly = 10;
+var sinhhoc = 8;
+var dtb = (hoahoc + vatly + sinhhoc) / 3;
+alert(dtb);
+document.write('Điểm trung bình của ba môn học là : ' +dtb);
